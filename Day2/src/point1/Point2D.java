6b8d0a6d00cb52381d4455ac4dfa75b38d738f27
normiecoder01 <132723499+normiecoder01@users.noreply.github.com>
2023-10-10 18:36:18 +0530

    Add files via upload

---
+++ Day2/src/point1/Point2D.java
@@ -0,0 +1,10 @@
+package point1;
+
+public class Point2D {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Day2/src/point1/test.java
@@ -0,0 +1,10 @@
+package point1;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
